bdc0d355848024eabb09fa5494814fa563c01af7
Vijaya Raghavan <49751108+Vijayraven95@users.noreply.github.com>
2020-06-16 10:31:12 +0200

    Add files via upload

---
+++ CycleGAN-tensorflow/utils.py
@@ -0,0 +1,156 @@
+"""
+Some codes from https://github.com/Newmu/dcgan_code
+"""
+from __future__ import division
+import math
+import pprint
+import scipy.misc
+import numpy as np
+import copy
+import matplotlib.image as mpimg 
+import matplotlib.pyplot as plt 
+from skimage.transform import resize
+from skimage import data
+import imageio
+import cv2 
+import matplotlib
+try:
+    _imread = mpimg.imread
+except AttributeError:
+    from imageio import imread as _imread
+
+pp = pprint.PrettyPrinter()
+
+get_stddev = lambda x, k_h, k_w: 1/math.sqrt(k_w*k_h*x.get_shape()[-1])
+
+# -----------------------------
+# new added functions for cyclegan
+class ImagePool(object):
+    def __init__(self, maxsize=50):
+        self.maxsize = maxsize
+        self.num_img = 0
+        self.images = []
+
+    def __call__(self, image):
+        if self.maxsize <= 0:
+            return image
+        if self.num_img < self.maxsize:
+            self.images.append(image)
+            self.num_img += 1
+            return image
+        if np.random.rand() > 0.5:
+            idx = int(np.random.rand()*self.maxsize)
+            tmp1 = copy.copy(self.images[idx])[0]
+            self.images[idx][0] = image[0]
+            idx = int(np.random.rand()*self.maxsize)
+            tmp2 = copy.copy(self.images[idx])[1]
+            self.images[idx][1] = image[1]
+            return [tmp1, tmp2]
+        else:
+            return image
+
+def load_test_data(image_path, fine_size=256):
+    img = imread(image_path)
+    img = resize(img, [fine_size, fine_size]) 
+
+    img = img/127.5 - 1
+    return img
+
+def load_train_data(image_path, load_size=286, fine_size=256, is_testing=False):
+    img_A = imread(image_path[0])
+    print(img_A.shape)
+    img_B = imread(image_path[1])
+    if not is_testing:
+        print ('resizesize', img_A.shape)
+        img_A = resize(img_A, [load_size, load_size])
+        img_B = resize(img_B, [load_size, load_size])
+        h1 = int(np.ceil(np.random.uniform(1e-2, load_size-fine_size)))
+        w1 = int(np.ceil(np.random.uniform(1e-2, load_size-fine_size)))
+        img_A = img_A[h1:h1+fine_size, w1:w1+fine_size]
+        img_B = img_B[h1:h1+fine_size, w1:w1+fine_size]
+
+        if np.random.random() > 0.5:
+            img_A = np.fliplr(img_A)
+            img_B = np.fliplr(img_B)
+    else:
+        img_A = resize(img_A, [fine_size, fine_size])
+        img_B = resize(img_B, [fine_size, fine_size])
+
+    img_A = img_A/127.5 - 1.
+    img_B = img_B/127.5 - 1.
+    print(img_A.shape,img_B.shape)
+    img_AB = np.concatenate((img_A, img_B), axis=2)
+    # img_AB shape: (fine_size, fine_size, input_c_dim + output_c_dim)
+    return img_AB
+
+# -----------------------------
+
+def get_image(image_path, image_size, is_crop=True, resize_w=64, is_grayscale = False):
+    return transform(imread(image_path, is_grayscale), image_size, is_crop, resize_w)
+
+def save_images(images, size, image_path):
+    print(images.shape)
+    print(size)
+    print(image_path)
+    im =inverse_transform(images)
+    print (im.shape)
+    # im=resize(im, [1024, 2048])
+    # scale_percent1 = 100
+    # scale_percent2 = 200 # percent of original size
+    # width = int(im.shape[1] * scale_percent1 / 100)
+    # height = int(im.shape[0] * scale_percent2 / 100)
+    dim = (im.shape[1], im.shape[2])
+    print(dim)
+    # resize image
+    # re= resize(im, [256, 256])
+    # imsave(inverse_transform(images), size, image_path)
+    im= im.reshape(im.shape[1],im.shape[2],3)
+    im= imageio.imwrite(image_path,im)
+    # im= cv2.imwrite(image_path, cv2.cvtColor(re, cv2.COLOR_RGB2BGR)) 
+    return im
+
+def imread(path, is_grayscale = False):
+    
+    return _imread(path).astype(np.float)
+
+def merge_images(images, size):
+    return inverse_transform(images)
+
+def merge(images, size):
+    h, w = images.shape[1], images.shape[2]
+    img = np.zeros((h * size[0], w * size[1], 3))
+    for idx, image in enumerate(images):
+        i = idx % size[1]
+        j = idx // size[1]
+        img[j*h:j*h+h, i*w:i*w+w, :] = image
+
+    return img
+
+def imsave(images, size, path):
+    
+
+    
+    return matplotlib.image.imsave(path, merge(images, size))
+
+def center_crop(x, crop_h, crop_w,
+                resize_h=64, resize_w=64):
+  if crop_w is None:
+    crop_w = crop_h
+  h, w = x.shape[:2]
+  j = int(round((h - crop_h)/2.))
+  i = int(round((w - crop_w)/2.))
+  return resize(
+      x[j:j+crop_h, i:i+crop_w], [resize_h, resize_w])
+
+def transform(image, npx=64, is_crop=True, resize_w=64):
+    # npx : # of pixels width/height of image
+    if is_crop:
+        cropped_image = center_crop(image, npx, resize_w=resize_w)
+    else:
+        cropped_image = image
+    return np.array(cropped_image)/127.5 - 1.
+
+def inverse_transform(images):
+    images = (images+1.)/2.
+    
+    return np.array(images)
